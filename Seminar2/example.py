7bfa7e86671656a8b990619e7429ddd87d8a6ef7
Igor Isupov <igor-i.s1@ya.ru>
2023-05-25 20:52:42 +0300

    Семинар 2

---
+++ Seminar2/example.py
@@ -0,0 +1,23 @@
+# # n = int(input())
+# # while n != -1:
+# #     print(n)
+# #     n = int(input())
+
+# while True:
+#     n = int(input())
+#     print(n)
+#     if n == -1:
+#         break
+
+
+#     for i in range(10, 15):
+#     print(i, 'привет')
+# /////////////////////////////////////////////////
+# for x in ["привет", "мир", "как", "дела"]:
+#     print(x)
+
+# for i in "мама":
+#     print(i)
+
+# for i, x in enumerate(["привет", "мир", "как", "дела"]):
+#     print(i, x)
