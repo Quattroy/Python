c84e74d9f19aa063b11d751407be8e28dcb6566c
Igor Isupov <igor-i.s1@ya.ru>
2023-06-13 22:46:12 +0300

    Семинар 8

---
+++ Seminar8/task_49.py
@@ -0,0 +1,58 @@
+# ; Задача №49. Решение в группах
+# ; Создать телефонный справочник с
+# ; возможностью импорта и экспорта данных в
+# ; формате .txt. Фамилия, имя, отчество, номер
+# ; телефона - данные, которые должны находиться
+# ; в файле.
+# ; 1. Программа должна выводить данные
+# ; 2. Программа должна сохранять данные в
+# ; текстовом файле
+# ; 3. Пользователь может ввести одну из
+# ; характеристик для поиска определенной
+# ; записи(Например имя или фамилию
+# ; человека)
+# ; 4. Использование функций. Ваша программа
+# ; не должна быть линейной
+
+file_path = "file.txt"
+
+
+def show_all_records():
+    with (open(file_path, 'r', encoding="utf-8")) as _data:
+        for line in _data:
+            phonebook_data = line.replace(";", " ")
+            print(phonebook_data, end="")
+
+
+def search_record(searching_data):
+    with open(file_path, "r", encoding="utf-8") as f:
+        for line in f:
+            if searching_data.lower() in line.split(";")[0].lower():
+                print(line, end="")
+
+
+def add_contact(new_contact_fio, new_contact_number):
+
+    with open("file.txt", "a", encoding="utf-8") as f:
+        f.write("\n")
+        f.write(new_contact_fio.replace(" ",";"))
+        f.write(';')
+        f.write(new_contact_number)
+
+
+def main():
+    print("Выберите действие: 1 - Показать справочник,"
+          "2 - найти контакт,"
+          "3 - добавить контакт")
+    select = int(input())
+    if select == 1:
+        show_all_records()
+    elif select == 2:
+        name = input("Введите фамилию: ")
+        search_record(name)
+    elif select == 3:
+        fio = input("Введите ФИО через пробел: ")
+        number = input("Введите Номер: ")
+        add_contact(fio, number)
+
+main()
