54c61306e53753e750726b54ddf1f73fe39b24d9
Igor Isupov <igor-i.s1@ya.ru>
2023-06-15 22:48:59 +0300

    Семинар

--- Seminar8/task_49.py
+++ Seminar8/task_49.py
@@ -35,15 +35,40 @@ def add_contact(new_contact_fio, new_contact_number):
 
     with open("file.txt", "a", encoding="utf-8") as f:
         f.write("\n")
-        f.write(new_contact_fio.replace(" ",";"))
+        f.write(new_contact_fio.replace(" "," "))
         f.write(';')
         f.write(new_contact_number)
+
+def record_name(nameOld, nameNew, newNumber):
+    with open('file.txt', 'r') as f1, open('file.txt', 'w') as f2:
+        lines = f1.readlines()
+        for line in lines:
+            line = line.strip()
+            if line == nameOld:
+                f2.write(nameNew)
+            else:
+                f2.write(line)
+    
+    
+    # with open ('file.txt', 'r') as f:
+    #      for line in f:
+    #         if old_name.lower() in line.split(";")[0].lower():
+    #             old_name = f.read()
+    #             new_name = old_name.replace(nameOld, nameNew)
+    # with open ('test.txt', 'w') as f:
+    #     f.write(new_name)
+    # # with open ('file.txt', 'r') as f:
+    # #     old_name = f.read()
+    # #     new_name = old_name.replace(nameOld, nameNew)
+    # # with open ('test.txt', 'w') as f:
+    # #     f.write(new_name)
 
 
 def main():
     print("Выберите действие: 1 - Показать справочник,"
           "2 - найти контакт,"
-          "3 - добавить контакт")
+          "3 - добавить контакт" "4 - изменить контакт",
+"5 - удалить контакт")
     select = int(input())
     if select == 1:
         show_all_records()
@@ -54,5 +79,12 @@ def main():
         fio = input("Введите ФИО через пробел: ")
         number = input("Введите Номер: ")
         add_contact(fio, number)
-
+    elif select == 4:
+        nameOld = input("Введите ФИО искомого контакта через пробел: ")
+        nameNew = input("Введите измененные ФИО контакта: ")
+        newNumber = input("Введите новый номер контакта: ")
+        record_name(nameOld, nameNew, newNumber)
+    elif select == 5:
+        name = input("Введите ФИО через пробел: ")
+        delite_name(name)
 main()
